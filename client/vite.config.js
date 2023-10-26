e1fe6c10e5ab70d37a95ecadd723563250ab3f1a
Javani Wright <javaniwright@gmail.com>
2023-10-26 15:55:07 -0400

    configured port

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,15 @@ import { resolve } from "path";
 export default defineConfig({
   build: {
     outDir: "dist",
-
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         grades: resolve(__dirname, "grades.html"),
       },
     },
+  },
+  server: {
+    host: true,
+    port: 80,
   },
 });
